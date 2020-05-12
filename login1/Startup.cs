630a87d7baa88230e30478a00b0f28444bf5df44
vanshikaa1009 <vanshikaaa1009@gmail.com>
2020-05-12 21:44:46 +0530

    Loginfolder

---
+++ login1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(login1.Startup))]
+namespace login1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
